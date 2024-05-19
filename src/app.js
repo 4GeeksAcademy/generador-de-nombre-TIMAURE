4e8e3867924be7409b02d875c5edec7017c4a4b2
Jhonny Timaure <164609178+TIMAURE@users.noreply.github.com>
2024-05-19 06:50:49 +0000

    actualizacionDomingo

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["auto", "perro", "gato"];
+  let accion = ["serviciodemecanica", "delasmudansas", "restaurat"];
+  let donde = ["sevilla", "madrid", "accion"];
+
+  function generateDominio() {
+    let randomquien = quien[Math.floor(Math.random() * quien.length)];
+    let randomaccion = accion[Math.floor(Math.random() * accion.length)];
+    let randomdonde = donde[Math.floor(Math.random() * donde.length)];
+
+    const dominio = `${randomquien}${randomaccion}${randomdonde}.Com.es`;
+    console.log(document.getElementById("dominio"));
+    document.getElementById("dominio").innerHTML = dominio;
+  }
+  document
+    .getElementById("generateDominioBtn")
+    .addEventListener("click", generateDominio);
+  console.log("Generador de excusas inicializado!");
 };
